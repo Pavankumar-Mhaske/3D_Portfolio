0c978e21e131c550118b10bc834b9042419bad7b
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-10 02:06:15 +0530

    chore: added links for different sections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center pt-5 fixed top-0 z-20 bg-primary`}
+      className={`w-full ${styles.paddingX}  flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -21,10 +21,26 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
-            Pavankumar{" "}
+            Pavankumar
             <span className="sm:block hidden">| JavaScript Mastry</span>
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              }
+            hover:text-white text-[18px] font-medium cursor-pointer
+              `}
+              onClick={()=>setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div></div>
       </div>
     </nav>
   );
