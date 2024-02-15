6fe051bd2592bf335d2aeaaa4bb33a5cbdfc41ea
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-15 11:42:48 +0530

    chore: navbar is upto date

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
             <span className="sm:block hidden">| JS Mastry</span>
           </p>
         </Link>
-        
+
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
             <li
