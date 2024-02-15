d0f9d7d1379e2b21bb5dfc895bc3bd2aad78502f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-15 10:14:30 +0530

    chore: we can show the stars in others sections as well

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
+        {/* <StarsCanvas /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
