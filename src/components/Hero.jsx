7f0bfd7b30f7c95a853e42be3ecece797b775722
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-16 00:14:07 +0530

    chore: updating the sub title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,9 +18,9 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">P1kumar</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Software developer... Crafting digital{" "}
+            Software developer... (Crafting digital{" "}
             <br className="sm:block hidden" />
-            experiences with passion and precision
+            experiences with passion and precision)
           </p>
         </div>
       </div>
