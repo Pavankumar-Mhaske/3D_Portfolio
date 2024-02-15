3d8cce987b2715a7fc4dd3055c074f46723e054f
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-15 17:47:47 +0530

    chore: changed the dummy name to original name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Pavankumar &nbsp;
-            <span className="sm:block hidden">| JS Mastry</span>
+            <span className="sm:block hidden">| Mhaske</span>
           </p>
         </Link>
 
